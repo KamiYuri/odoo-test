eab5f93d375e5e53ed5ca66aa57e2f3e6e88883e
Dung Hoang Viet <dung.hv194254@sis.hust.edu.vn>
2023-04-01 09:56:21 +0700

    append html into odoo view

--- module-test/controllers/controllers.py
+++ module-test/controllers/controllers.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Module-test(http.Controller):
-#     @http.route('/module-test/module-test', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Controller(http.Controller):
 
-#     @http.route('/module-test/module-test/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('module-test.listing', {
-#             'root': '/module-test/module-test',
-#             'objects': http.request.env['module-test.module-test'].search([]),
-#         })
+    @http.route('/module-test/hello', auth='public', type='json')
+    def hello(self):
+        return {
+            'html': """
+                    <div>
+                        <h1>hello, world</h1>
+                    </div> 
+                    """
+        }
 
-#     @http.route('/module-test/module-test/objects/<model("module-test.module-test"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('module-test.object', {
-#             'object': obj
-#         })
+    # @http.route('/module-test/module-test', auth='public')
+    # def index(self, **kw):
+    #     return "Hello, world"
+    #
+    # @http.route('/module-test/module-test/objects', auth='public')
+    # def list(self, **kw):
+    #     return http.request.render('module-test.listing', {
+    #         'root': '/module-test/module-test',
+    #         'objects': http.request.env['module-test.module-test'].search([]),
+    #     })
+    #
+    # @http.route('/module-test/module-test/objects/<model("module-test.module-test"):obj>', auth='public')
+    # def object(self, obj, **kw):
+    #     return http.request.render('module-test.object', {
+    #         'object': obj
+    #     })
